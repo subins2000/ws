9229a7a22ebc44e2fe2db7da59fc4576b1607eea
Subin Siby <subins2000@gmail.com>
2015-06-29 14:19:07 +0530

    Cron JOB and no cron.php in bg

--- cron.php
+++ cron.php
@@ -1,8 +1,4 @@
 <?php
-$endTime = time() + 100;
-while (true) {
-  if (time() >= $endTime && file_get_contents(__DIR__ . "/active.txt") == "0\n") {
-    exec("php " . __DIR__ . "/index.php");
-  }
-  sleep(30);
+if (file_get_contents(__DIR__ . "/active.txt") == "0\n") {
+  exec("php " . __DIR__ . "/index.php");
 }
--- index.php
+++ index.php
@@ -5,7 +5,6 @@ require_once __DIR__ . "/config.php";
  * Kill existing WS servers
  */
 exec("kill $(ps aux | grep 'bg.php' | awk '{print $2}')");
-exec("kill $(ps aux | grep 'cron.php' | awk '{print $2}')");
 file_put_contents(__DIR__ . "/active.txt", "0\n"); // No Active Connections as server was killed
 
 sleep(3);
@@ -13,7 +12,6 @@ sleep(3);
  * The WebSocket server is not started. So we, start it
  */
 exec("nohup php -q '$docRoot/bg.php' > /dev/null 2>&1 &");
-exec("nohup php -q '$docRoot/cron.php' > /dev/null 2>&1 &");
 ?>
 <!DOCTYPE html>
 <html>
