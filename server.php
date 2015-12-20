3c1f5b791251bbba74fa5816c57409733312b7fc
subins2000 <subins2000@gmail.com>
2015-12-20 22:46:41 +0530

    Some uopdates

--- server.php
+++ server.php
@@ -4,7 +4,7 @@ use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
   
 if(isset($startNow)){
-  $ip = getenv("OPENSHIFT_PHP_IP") ?:"127.0.0.1";
+  $ip = getenv("OPENSHIFT_PHP_IP") ?:"192.168.1.2";
   $port = getenv("OPENSHIFT_PHP_PORT") ?:"8000";
   require_once "$docRoot/vendor/autoload.php";
   require_once "$docRoot/class.base.php";
